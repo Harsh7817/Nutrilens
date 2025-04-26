cc7ae0eaddf0ad2ad213f3d5c98a3c1555e96c54
Harsh7817 <harshitbaliyan116@gmail.com>
2025-04-26 15:10:09 +0530

    Added dashboard folder

---
+++ dashboard/dashboard.js
@@ -0,0 +1,8 @@
+// Add event listeners to buttons
+document.getElementById("scanFoodBtn").addEventListener("click", function () {
+    alert("Scan Food button clicked!");
+  });
+  
+  document.getElementById("uploadImageBtn").addEventListener("click", function () {
+    alert("Upload Image button clicked!");
+  });
